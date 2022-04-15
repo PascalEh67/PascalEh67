99dea01f15815d8f936e1aab1af2667c7de18f4d
PascalEh67 <pascal.eh67@gmail.com>
2022-04-15 15:47:27 +0200

    ok

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World 2 !");
+Console.Title="titre";
+
+
+String txt="ok";
+float val=3.14f;
+
+Console.WriteLine(txt);
+
+int nombre=245;
+Console.WriteLine((nombre ++));
+Console.WriteLine(nombre ++);
+Console.WriteLine(val);
 Console.WriteLine("Hello, World!");
